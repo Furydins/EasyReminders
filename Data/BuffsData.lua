2e686ef0d0e22e872ebb6c0c8f78a1826d587c5a
Furydin <35685316+Furydins@users.noreply.github.com>
2026-03-09 14:56:53 +0000

    Mark of the Wild bugfix

--- Data/BuffsData.lua
+++ Data/BuffsData.lua
@@ -34,7 +34,7 @@ EasyReminders.Data.Buffs = {
     [6673] = {["buffID"] = 6673, ["class"] = "WARRIOR"},
     [21562] = {["buffID"] = 21562, ["class"] = "PRIEST"},
     [1459] = {["buffID"] = 1459, ["class"] = "MAGE"},
-    [1125] = {["buffID"] = 1126, ["class"] = "DRUID"},
+    [1126] = {["buffID"] = 1126, ["class"] = "DRUID"},
     [462854] = {["buffID"] = 462854, ["class"] = "SHAMAN"},
     [381748] = {["buffID"] = 381748, ["class"] = "EVOKER", ["additionalBuffs"] = {381758,381732,381741,381746,381748,381750,381749,381751,381752,381753,381754,381756,381757}},
 
