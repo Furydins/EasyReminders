cdf4e29c24d07e822e5b7cffa00f2912b7b75518
Furydin <35685316+Furydins@users.noreply.github.com>
2026-04-07 17:05:28 +0100

    - Added option to limit tracking to seasonal Myhtic dungeons only - Stop using outside options for disabled dungeons/raids

--- TrackingUtils.lua
+++ TrackingUtils.lua
@@ -5,7 +5,8 @@ local TrackingUtils = EasyReminders.TrackingUtils
 local function IsDungeonInCurrentSeason(targetMapID)
     C_MythicPlus.RequestMapInfo()
     local activeMaps = C_ChallengeMode.GetMapTable()
-    for _, mapID in ipairs(activeMaps) do
+    for _, challengeModeID in ipairs(activeMaps) do
+        local _, _, _, _, _, mapID = C_ChallengeMode.GetMapUIInfo(challengeModeID)
         if mapID == targetMapID then
             return true
         end
@@ -14,20 +15,20 @@ local function IsDungeonInCurrentSeason(targetMapID)
 end
 
 function TrackingUtils:IsDungeonInSeason()
-  local _, instanceType, _, _, _, _, _, _, _, instanceMapID = _G.GetInstanceInfo()
+  local _, instanceType, _, _, _, _, _, instanceID, _, _ = _G.GetInstanceInfo()
 
-  return instanceType == "party" and IsDungeonInCurrentSeason(instanceMapID)
+  return instanceType == "party" and IsDungeonInCurrentSeason(instanceID)
   
 end
 
 function TrackingUtils:SelectTrackingList(outside, delve, dungeon, raid, pvp)
   
   local trackingList = nil
-  local _, instanceType, difficultyID, _, _, _, _, _, _, _ = _G.GetInstanceInfo()
+  local _, instanceType, difficultyID, _, _, _, _, _, _, mapID = _G.GetInstanceInfo()
   local _, _, isHeroic, isChallengeMode, displayHeroic, displayMythic, _, isLFR, _, _ = _G.GetDifficultyInfo(difficultyID)
 
   if "raid" == instanceType then
-    trackingList = outside
+    trackingList = {} 
     if EasyReminders.globalDB.minimumRaidDifficulty == "LFR" then
       trackingList = raid
     elseif EasyReminders.globalDB.minimumRaidDifficulty == "NORMAL" and (not isLFR) then
@@ -38,19 +39,21 @@ function TrackingUtils:SelectTrackingList(outside, delve, dungeon, raid, pvp)
       trackingList = raid
     end
     if C_Loot.IsLegacyLootModeEnabled() and EasyReminders.globalDB.ignoreLegacyRaids then
-      trackingList = outside
+      trackingList = {}
     end
   elseif "party" == instanceType then
-     trackingList = outside
+     trackingList = {} 
     if EasyReminders.globalDB.minimumDungeonDifficulty == "NORMAL" then
       trackingList = dungeon
     elseif EasyReminders.globalDB.minimumDungeonDifficulty == "HEROIC" and (displayHeroic or displayMythic) then
       trackingList = dungeon
     elseif EasyReminders.globalDB.minimumDungeonDifficulty == "MYTHIC" and (displayMythic) then
       trackingList = dungeon
+    elseif EasyReminders.globalDB.minimumDungeonDifficulty == "MYTHIC_SEASONAL" and displayMythic and EasyReminders.TrackingUtils:IsDungeonInSeason() then
+      trackingList = dungeon
     end 
     if C_Loot.IsLegacyLootModeEnabled() and EasyReminders.globalDB.ignoreLegacyDungeons and (not displayMythic or not EasyReminders.TrackingUtils:IsDungeonInSeason()) then
-      trackingList = outside
+      trackingList = {}
     end
   elseif "pvp" == instanceType then 
     trackingList = pvp
