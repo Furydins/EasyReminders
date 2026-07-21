fd6498939606fa10bc79df8ba27b0b3bbc0ebf4c
Furydin <35685316+Furydins@users.noreply.github.com>
2026-07-21 23:02:53 +0100

    Avoid checking player class during combat, PvP or M+ lockdowns to avoid "secret value" issues

--- BuffCheck.lua
+++ BuffCheck.lua
@@ -48,12 +48,13 @@ function BuffCheck:CheckBuffs(missingBuffs)
 
   local trackingList = {}
    
-  local _, class, _ = _G.UnitClass("player")
 
   local trackingList = EasyReminders.TrackingUtils:SelectTrackingList(TrackingList.outside, TrackingList.delve, TrackingList.dungeon, TrackingList.raid, TrackingList.pvp)
 
   if not _G.InCombatLockdown() and not C_ChallengeMode.IsChallengeModeActive() 
       and not C_PvP.IsMatchActive() and not (C_Secrets and C_Secrets.ShouldAurasBeSecret()) then
+
+      local _, class, _ = _G.UnitClass("player")
      local foundbuffs = {}
 
      _G.AuraUtil.ForEachAura("player", "HELPFUL", nil, function(_, _, _, _, duration, expires, _, _, _, spellID)
