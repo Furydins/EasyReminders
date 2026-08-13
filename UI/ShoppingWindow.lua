8fade21bda981e74288db387111369fea24b8eae
Furydin <35685316+Furydins@users.noreply.github.com>
2026-08-13 11:58:53 +0100

    Need an even bigger delay.

--- UI/ShoppingWindow.lua
+++ UI/ShoppingWindow.lua
@@ -114,7 +114,7 @@ function ShoppingWindow:UpdateNotifications(type)
     -- Delay login check to allow time
     -- to cache bag contents
     if _type== "LOGIN" then
-        loginPending = 5
+        loginPending = 6
         return
     elseif loginPending > 1 then
         if _type == "TIMER" then
