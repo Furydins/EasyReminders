0f9b2c8abc2a824e66d5ffe61c963fcd4aca60cc
Furydin <35685316+Furydins@users.noreply.github.com>
2026-09-09 22:26:21 +0100

    Treat the new World Difficulty as LFR

--- TrackingUtils.lua
+++ TrackingUtils.lua
@@ -31,7 +31,7 @@ function TrackingUtils:SelectTrackingList(outside, delve, dungeon, raid, pvp)
     trackingList = {} 
     if EasyReminders.globalDB.minimumRaidDifficulty == "LFR" then
       trackingList = raid
-    elseif EasyReminders.globalDB.minimumRaidDifficulty == "NORMAL" and (not isLFR) then
+    elseif EasyReminders.globalDB.minimumRaidDifficulty == "NORMAL" and not (isLFR or difficultyID == 250) then
       trackingList = raid
     elseif EasyReminders.globalDB.minimumRaidDifficulty == "HEROIC" and (displayHeroic or displayMythic) then
       trackingList = raid
